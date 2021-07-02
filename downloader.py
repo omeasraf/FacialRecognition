626b7be643bcd13640cce55577576f33e5d625d9
Ome Asraf <omeasraf0@gmail.com>
2021-07-02 13:48:53 -0400

    Create downloader.py
    Automatically download images from google within minutes

---
+++ downloader.py
@@ -0,0 +1,27 @@
+from simple_image_download import simple_image_download
+import multiprocessing
+# from names import names
+
+
+# A better way of doing this would be to use the face_recognition to identify all the faces and make sure there aren't multiple faces in a single image
+# Todo: Add checks for faces also match against a sample image
+def main():
+    response = simple_image_download()
+
+    names = ["Olivia Rodrigo", "Taylor Swift",
+             "Ed Sheeran", "Selena Gomez", "Miley Cyrus", "Steve Jobs", "Tim Cook", "Bill Gates", "Zayn Malik", "Harry Styles", "Louis Tomlinson", "Liam Payne", "Gigi Hadid", "Niall Horan", "Kendall Jenner"]
+    for name in names:
+        t1 = multiprocessing.Process(target=response.download, args=(
+            name, name, 200, {'.jpg', '.png', '.jiff', '.gif', '.jpeg'}))
+        t2 = multiprocessing.Process(target=response.download, args=(
+            name + " face", name, 200, {'.jpg', '.png', '.jiff', '.gif', '.jpeg'}, True))
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+
+if __name__ == '__main__':
+    main()
